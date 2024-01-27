7469b9980ecb069165f1a41bdd819be476d01320
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2024-01-28 04:17:34 +0530

    commit

--- public/scrip.js
+++ public/scrip.js
@@ -39,6 +39,68 @@ function func(){
 
 
 
+var button = document.getElementById('rights');
+button.onclick = function () {
+    var container = document.getElementById('dgnotes');
+    sideScroll(container,'right',15,200,10);
+};
+
+var back = document.getElementById('lefts');
+back.onclick = function () {
+    var container = document.getElementById('dgnotes');
+    sideScroll(container,'left',15,200,10);
+};
+
+function sideScroll(element,direction,speed,distance,step){
+    scrollAmount = 0;
+    var slideTimer = setInterval(function(){
+        if(direction == 'left'){
+            element.scrollLeft -= step;
+        } else {
+            element.scrollLeft += step;
+        }
+        scrollAmount += step;
+        if(scrollAmount >= distance){
+            window.clearInterval(slideTimer);
+        }
+    }, speed);
+}
+
+
+
+
+var button1 = document.getElementById('rights1');
+button1.onclick = function () {
+    var container = document.getElementById('oncourses');
+    sideScroll(container,'right',15,200,10);
+};
+
+var back1 = document.getElementById('lefts1');
+back1.onclick = function () {
+    var container = document.getElementById('oncourses');
+    sideScroll(container,'left',15,200,10);
+};
+
+function sideScroll(element,direction,speed,distance,step){
+    scrollAmount = 0;
+    var slideTimer = setInterval(function(){
+        if(direction == 'left'){
+            element.scrollLeft -= step;
+        } else {
+            element.scrollLeft += step;
+        }
+        scrollAmount += step;
+        if(scrollAmount >= distance){
+            window.clearInterval(slideTimer);
+        }
+    }, speed);
+}
+
+
+
+
+
+
 
 
 
