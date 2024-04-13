f1cda4ca7cb9db7a91d68f94b6224bfd0705069b
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2024-04-13 08:19:18 +0530

    a

---
+++ src/component/Footer/Footer.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+            <footer>
+
+                <div>
+                    <p>Copyright@2024 Shivam Patanwar</p>
+                    <p>Developed by Shivam Patanwar</p>
+                    <p>Graphic Design by <a href="https://yashgraphics.com" rel="noopener" target="_blank">Yashgraphics.com</a></p>
+
+                    <a href="https://www.instagram.com/shivampatanwardeveloper" target="_blank" rel="noopener"> <img class="footimg" src="https://firebasestorage.googleapis.com/v0/b/shivampatanwarapp.appspot.com/o/svg%2Finstagram.svg?alt=media&token=78a56b1f-300d-43a2-acac-90d3d3002757" alt="instagram" /></a>
+                    <a href="https://twitter.com/shivampatanwar" target="_blank" rel="noopener"><img class="footimg" src="https://firebasestorage.googleapis.com/v0/b/shivampatanwarapp.appspot.com/o/svg%2Ftwitter-x.svg?alt=media&token=ae765a64-8efe-4769-827a-7433b0c5eb68" alt="twitter-x" /></a>
+                    <a href="https://www.facebook.com/shivampatanwardeveloper" target="_blank" rel="noopener"> <img class="footimg" src="https://firebasestorage.googleapis.com/v0/b/shivampatanwarapp.appspot.com/o/svg%2Ffacebook.svg?alt=media&token=ea01ebad-eb19-449e-b7ec-f5df07427edd" alt="facebook" /></a>
+                    <a href="https://www.youtube.com/@shivampatanwardeveloper" target="_blank" rel="noopener"><img class="footimg" src="https://firebasestorage.googleapis.com/v0/b/shivampatanwarapp.appspot.com/o/svg%2Fyoutube.svg?alt=media&token=b138e205-b8be-4e7b-a65e-7d70ea234019" alt="youtube" /></a>
+                    <a href="mailto: shivampatanwar@gmail.com" target="_blank" rel="noopener"><img class="footimg" src="https://firebasestorage.googleapis.com/v0/b/shivampatanwarapp.appspot.com/o/svg%2Femail.svg?alt=media&token=e2151d2b-79d5-4482-a23e-9bd67bb894ac" alt="email" /></a>
+                    <a href="https://maps.app.goo.gl/QGDWiG3mY2x3ee9N9" target="_blank" rel="noopener"><img class="footimg" src="https://firebasestorage.googleapis.com/v0/b/shivampatanwarapp.appspot.com/o/svg%2Fmap.svg?alt=media&token=410ac9ab-43a0-4cb6-b099-70523b83a391" alt="map" /></a>
+
+
+                </div>
+
+
+                <div>
+                    <h1 class="footh1">Youtube Channel</h1>
+                    <a href="https://www.youtube.com/@shivampatanwardeveloper" target="_blank" rel="noopener">Developer</a>
+                    <a href="https://www.youtube.com/@shivampatanwarmotivation" target="_blank" rel="noopener">Motivation</a>
+                    <a href="https://www.youtube.com/@shivampatanwareducation" target="_blank" rel="noopener">Education</a>
+                </div>
+
+
+                <div>
+                    <h1 class="footh1">Social Media</h1>
+                    <a href="https://www.instagram.com/shivampatanwardeveloper" target="_blank" rel="noopener">Instagram</a>
+                    <a href="https://twitter.com/shivampatanwar" target="_blank" rel="noopener">Twitter</a>
+                    <a href="https://www.facebook.com/shivampatanwardeveloper" target="_blank" rel="noopener">Facebook Page</a>
+                    <a href="https://in.linkedin.com/in/shivampatanwar" target="_blank" rel="noopener">Linkdin</a>
+                </div>
+
+
+                <div>
+                    <h1 class="footh1">Contact</h1>
+                    <a href="mailto: shivampatanwar@gmail.com">Email</a>
+                    <a href="#">Get in touch</a>
+                    <a href="https://wa.me/9685430664?text=I'm%20inquiring%20about%20the%20Developement" rel="noopener" target="_blank">Whatsapp</a>
+
+                </div>
+
+            </footer>
+        </>
+    )
+}
+
+export default Footer
---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <nav>
+        
+      </nav>
+    </div>
+  )
+}
+
+export default Header
